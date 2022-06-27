f6f6c8d2012fa8f56341b7ec46d39e9cab8a6f8d
William Casarin <jb55@jb55.com>
2022-06-26 22:20:31 -0700

    input fields working
    Signed-off-by: William Casarin <jb55@jb55.com>

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+let STATE
+
+async function go() {
+	const state = {}
+
+	const params = get_qs()
+	const data = params.get("d");
+	const edit = +params.get("edit") === 1;
+
+	if (edit) {
+		const decoded_state = STATE = decode_link_data(data)
+		handle_form(decoded_state)
+	} else if (data) {
+		handle_lnlink(data)
+	} else {
+		handle_form(state)
+	}
+}
+
 async function make_request({nodeid, address, method, rune, params}) {
 	const LNSocket = await lnsocket_init()
 	const ln = LNSocket()
@@ -5,63 +24,92 @@ async function make_request({nodeid, address, method, rune, params}) {
 	ln.genkey()
 	await ln.connect_and_init(nodeid, address)
 
-	const {result} = await ln.rpc({ rune, method, params })
+	const res = await ln.rpc({ rune, method, params })
 
 	ln.disconnect()
-	return result
-}
-
-function char_to_hex(cstr) {
-	const c = cstr.charCodeAt(0)
-	// c >= 0 && c <= 9
-	if (c >= 48 && c <= 57) {
-		return c - 48;
-	}
-	// c >= a && c <= f
- 	if (c >= 97 && c <= 102) {
-		return c - 97 + 10;
-	}
-	// c >= A && c <= F
- 	if (c >= 65 && c <= 70) {
-		return c - 65 + 10;
-	}
-	return -1;
+	return res
 }
 
 
-function hex_decode(str, buflen)
-{
-	let bufsize = buflen || 33
-	let c1, c2
-	let i = 0
-	let j = 0
-	let buf = new Uint8Array(bufsize)
-	let slen = str.length
-	while (slen > 1) {
-		if (-1==(c1 = char_to_hex(str[j])) || -1==(c2 = char_to_hex(str[j+1])))
-			return null;
-		if (!bufsize)
-			return null;
-		j += 2
-		slen -= 2
-		buf[i++] = (c1 << 4) | c2
-		bufsize--;
-	}
-
-	return buf
-}
-
-function make_invoice({nodeid, address, rune, description, msatoshi}) {
+function make_invoice({nodeid, label, address, rune, description, msatoshi}) {
 	return make_request({
 		method: "invoice",
-		nodeid, address, rune,
-		msatoshi: msatoshi,
-		label: `lnlink-${new Date().getTime()}`,
-		description: description
+		nodeid, address, rune, 
+		params: { label, msatoshi, description }
 	})
 }
 
-function make_qr() {
+function get_qs() {
+	return (new URL(document.location)).searchParams;
+}
+
+function handle_lnlink(data)
+{
+	const decoded = decode_link_data(data)
+	const el = document.querySelector("#content") 
+	STATE = {data: decoded}
+	el.innerHTML = render_lnlink(STATE)
+	unhide_content()
+}
+
+function slugify(str)
+{
+	return str.toLowerCase().replace(" ", "-")
+}
+
+function make_description({data})
+{
+	const {fields, description} = data
+	if (!fields) {
+		return description
+	}
+
+	return Object.keys(FIELDS_BITS).reduce((desc, name) => {
+		const el = document.querySelector(`#${name}_input`)
+		const val = el && el.value
+		if (val && (FIELDS_BITS[name] & fields))
+			desc += `\n${name}: "${val}"`
+		return desc
+	}, `${description}\n`)
+}
+
+async function click_buy_button()
+{
+	const {data} = STATE
+	const label = `lnlink-${slugify(data.product)}-${new Date().getTime()}`
+
+	const description = make_description(STATE)
+
+	const res = await make_invoice({
+		nodeid: hex_encode(data.nodeid),
+		label: label,
+		rune: base64_encode(data.rune),
+		description: description,
+		address: "ws://" + data.ip,
+		msatoshi: (+data.price) * 1000,
+	})
+
+	if (!(res && res.result)) {
+		show_error(res)
+		return
+	}
+
+	make_qrcode(res.result)
+}
+
+function show_error(err)
+{
+	const el = document.querySelector("#qrcode")
+	el.innerHTML = `
+	<p>Oh no :( there was a problem. Please try again</p>
+	<pre>${err}</pre>
+	`
+}
+
+function make_qrcode(invoice)
+{
+	const qrlink = document.querySelector("#qrcode-link")
+
 	const link = "LIGHTNING:" + invoice.bolt11.toUpperCase()
 	const qr = new QRCode("qrcode", {
 		text: link,
@@ -71,96 +119,181 @@ function make_qr() {
 		colorLight : "#ffffff",
 		correctLevel : QRCode.CorrectLevel.L
 	})
+
 	qrlink.href = link
-	qrlink.style.display = "block";
-	qrdiv.style.display = "block";
-	offerdata.value = invoice.bolt11
-	el.style.display = "none";
-	tipjar_img.style.display = "none";
 }
 
-
-async function click_make_invoice(el) {
-	const note = prompt("Leave a note!", "")
-
-	const invoice = await make_invoice(note)
+function capitalize(name) {
+	return name[0].toUpperCase() + name.slice(1)
 }
 
-async function copy_tip() {
-	const offer = document.querySelector("#offerdata").value.trim();
-	try {
-		await navigator.clipboard.writeText(offer)
-		alert("Invoice copied to clipboard!")
-	} catch(err) {
-		console.log("clipboard copy error", err)
-		document.querySelector("#offerdata").style.display = "block"
-	}
+function render_input_field(name) {
+	const label = capitalize(name)
+	return `
+	<div class="form-group">
+		<label for="${name}">${label}</label>
+		<input class="form-control" id="${name}_input" placeholder="${label}">
+	</div>
+	`
 }
 
-async function go() {
-	/*
-	const tipjar_copies = document.querySelectorAll(".tipjar-copy")
-	for (const el of tipjar_copies) {
-		el.onclick = copy_tip
-	}
-	*/
-
-	const data = window.location.hash.substr(1)
-	const state = {}
-
-	if (data) {
-		handle_lnlink(data)
-	} else {
-		handle_form(state)
-	}
-}
-
-function handle_lnlink(data)
+function render_input_form(fields)
 {
-	const decoded = decode_link_data(data)
-	const el = document.querySelector("#content") 
-	el.innerHTML = render_lnlink({data: decoded})
+	if (!fields)
+		return ""
+
+	const inputs = Object.keys(FIELDS_BITS).reduce((str, name) => {
+		if (fields & FIELDS_BITS[name])
+			str += render_input_field(name)
+		return str
+	}, "")
+
+	return `
+	<div id="form">
+		${inputs}
+	</div>
+	`
 }
 
 function render_lnlink(state)
 {
+
+	const data = state.data
+	const product = data.product || "For Sale!"
+	const sats = +data.price
+	const ending = sats === 1 ? "sat" : "sats"
+	const price = format_amount(+data.price) + " " + ending
+
 	return `
-	<pre>
-	${JSON.stringify(state.data, null, 2)}
-	</pre>
+	<div id="card">
+	<h3>${data.product}</h3>
+	  <p>${data.description}</p>
+	  <h1>${price}</h1>
+
+          ${render_input_form(data.fields)}
+
+	  <button type="button" class="btn btn-primary" onclick="click_buy_button()">Buy</button>
+
+	  <a id="qrcode-link" href="#">
+		  <div id="qrcode">
+		  </div>
+	  </a>
+	</div>
 	`
+}
+
+function update_fields_checkboxes(fields)
+{
+	if (fields === 0)
+		return
+
+	const keys = Object.keys(FIELDS_BITS)
+	for (const field of keys) {
+		document.querySelector("#"+field).checked = !!(fields & FIELDS_BITS[field])
+	}
+}
+
+function update_form_from_state(state)
+{
+	const state_keys = Object.keys(state)
+	for (const key of state_keys) {
+		switch (key) {
+		case "ip":
+		case "nodeid":
+			var el = document.getElementById("ip")
+			el.value = hex_encode(state.nodeid) + "@" + state.ip
+			break
+		case "rune":
+			var el = document.getElementById("rune")
+			el.value = base64_encode(state.rune)
+			break
+		case "fields":
+			update_fields_checkboxes(state.fields || 0)
+			break
+		default:
+			var el = document.getElementById(key)
+			if (!el)
+				throw new Error(`no el for state key ${key}`)
+			el.value = state[key]
+		}
+	}
+
+	if (state_keys.length > 0)
+		update_link(state)
+}
+
+function unhide_content() {
+	document.querySelector("#content").style.display = "block"
 }
 
 function handle_form(state)
 {
-	const inputs = document.querySelectorAll("#form input")
+	unhide_content()
+	update_form_from_state(state)
+
+	const inputs = document.querySelectorAll("#form input,textarea")
 	for (const input of inputs) {
 		input.addEventListener('input', input_changed.bind(null,state))
 	}
 }
 
+const FIELDS_BITS = {
+	email: 1 << 0,
+	address: 1 << 1,
+}
+
+function fields_changed(state, field, checked)
+{
+	state.fields = state.fields || 0
+	const bit = FIELDS_BITS[field]
+
+	if (checked)
+		state.fields |= bit
+	else
+		state.fields &= ~bit
+}
+
+
 function input_changed(state, ev)
 {
 	let ok = false
 	switch (ev.target.id) {
-	case "node_address":
-		ok = address_changed(state, ev)
+	case "ip":
+		ok = address_changed(state, ev.target.value)
 		break
 	case "rune":
-		ok = rune_changed(state, ev)
+		ok = rune_changed(state, ev.target.value)
 		break
-	case "product":
-		state.product = ev.target.value
+
+	case "email":
+	case "address":
+		fields_changed(state, ev.target.id, ev.target.checked)
 		ok = true
 		break
-	case "price":
-		state.price = ev.target.value
+	default:
+		state[ev.target.id] = ev.target.value
 		ok = true
-		break
 	}
 
 	if (ok)
 		update_link(state)
+}
+
+function tagged_u8(tag, data)
+{
+	const b = new Uint8Array(2)
+	b[0] = tag
+	b[1] = data
+	return b
+}
+
+function tagged_u32(tag, num)
+{
+	const b = new Uint8Array(5)
+	const view = new DataView(b.buffer)
+	b[0] = tag
+	view.setUint32(1, num)
+	return b
 }
 
 function tagged_array(tag, arr)
@@ -193,7 +326,9 @@ const TAG_IP = 2
 const TAG_RUNE = 3
 const TAG_PRODUCT = 4
 const TAG_PRICE = 5
-const NUM_TAGS = 5
+const TAG_DESCRIPTION = 6
+const TAG_FIELDS = 7
+const NUM_TAGS = 7
 const ALL_TAGS = (function() {
 	let a = []
 	for (let i = 1; i <= NUM_TAGS; i++) {
@@ -210,18 +345,10 @@ function tag_name(tag)
 	case TAG_RUNE: return 'rune'
 	case TAG_PRODUCT: return 'product'
 	case TAG_PRICE: return 'price'
+	case TAG_DESCRIPTION: return 'description'
+	case TAG_FIELDS: return 'fields'
 	}
 	throw new Error(`invalid tag: ${tag}`)
-}
-
-function name_to_tag(name) {
-	switch (name) {
-	case "nodeid": return TAG_NODEID
-	case "ip": return TAG_IP
-	case "rune": return TAG_RUNE
-	case "product": return TAG_PRODUCT
-	case "price": return TAG_PRICE
-	}
 }
 
 function tag_type(tag)
@@ -231,7 +358,9 @@ function tag_type(tag)
 	case TAG_IP: return 'string'
 	case TAG_RUNE: return 'array'
 	case TAG_PRODUCT: return 'string'
-	case TAG_PRICE: return 'string'
+	case TAG_PRICE: return 'u32'
+	case TAG_DESCRIPTION: return 'string'
+	case TAG_FIELDS: return 'u8'
 	}
 	throw new Error(`invalid tag: ${tag}`)
 }
@@ -250,6 +379,14 @@ function parse_tag(state)
 	if (tag >= TAG_NODEID && tag <= NUM_TAGS)
 		return tag
 	return null
+}
+
+function parse_u32_packet(state)
+{
+	let view = new DataView(state.buf.buffer)
+	const u32 = view.getUint32(state.pos)
+	state.pos += 4
+	return u32
 }
 
 function parse_array_packet(state)
@@ -303,6 +440,7 @@ function post_process_packet(tag, pkt)
 	switch (tag) {
 	case TAG_NODEID: return hex_encode(pkt)
 	case TAG_RUNE:   return base64_encode(pkt)
+	case TAG_IP:     return "ws://" + pkt
 	}
 	return pkt
 }
@@ -320,6 +458,14 @@ function parse_link_packet(state)
 		if (!(pkt = parse_array_packet(state)))
 			return false
 		break
+	case 'u32':
+		if (!(pkt = parse_u32_packet(state)))
+			return false
+		break
+	case 'u8':
+		if (null === (pkt = parse_byte(state)))
+			return false
+		break
 	case 'string':
 		if (!(pkt = parse_string_packet(state)))
 			return false
@@ -328,7 +474,7 @@ function parse_link_packet(state)
 		throw new Error(`invalid tag type: ${ttype}`)
 	}
 
-	state.data[tag_name(tag)] = post_process_packet(tag, pkt)
+	state.data[tag_name(tag)] = pkt
 	return true
 }
 
@@ -368,6 +514,12 @@ function encode_link_data(state)
 			case 'array':
 				tv = tagged_array(tag, val)
 				break
+			case 'u32':
+				tv = tagged_u32(tag, +val)
+				break
+			case 'u8':
+				tv = tagged_u8(tag, val)
+				break
 			}
 			acc.push(tv)
 		}
@@ -381,10 +533,16 @@ function encode_link_data(state)
 function update_link(state) {
 	const dat = encode_link_data(state)
 	const el = document.querySelector("#link")
-	const link = `http://lnlink.org/#${dat}`
+	const host = window.location.host
+	const scheme = window.location.protocol
+	const params = get_qs()
+	const edit = +params.get("edit") === 1 ? `&edit=1` : ""
+	const link = `${scheme}//${host}/?d=${dat}`
 
 	el.href = link
 	el.text = link
+
+	window.history.replaceState({}, '', link + edit)
 }
 
 function str_buffer(str) {
@@ -424,9 +582,8 @@ function base64_decode(str) {
 }
 
 
-function rune_changed(state, ev)
+function rune_changed(state, rune_str)
 {
-	const rune_str = ev.target.value
 	try {
 		state.rune = base64_decode(rune_str)
 		return true
@@ -436,9 +593,8 @@ function rune_changed(state, ev)
 	}
 }
 
-function address_changed(state, ev)
+function address_changed(state, address)
 {
-	const address = ev.target.value
 	const [nodeid, ip] = address.split("@")
 	const raw = hex_decode(nodeid)
 	if (!raw)
@@ -475,6 +631,46 @@ function time_delta(current, previous) {
     } else {
         return Math.round(elapsed/msPerYear ) + ' years ago';   
     }
+}
+
+function char_to_hex(cstr) {
+	const c = cstr.charCodeAt(0)
+	// c >= 0 && c <= 9
+	if (c >= 48 && c <= 57) {
+		return c - 48;
+	}
+	// c >= a && c <= f
+ 	if (c >= 97 && c <= 102) {
+		return c - 97 + 10;
+	}
+	// c >= A && c <= F
+ 	if (c >= 65 && c <= 70) {
+		return c - 65 + 10;
+	}
+	return -1;
+}
+
+
+function hex_decode(str, buflen)
+{
+	let bufsize = buflen || 33
+	let c1, c2
+	let i = 0
+	let j = 0
+	let buf = new Uint8Array(bufsize)
+	let slen = str.length
+	while (slen > 1) {
+		if (-1==(c1 = char_to_hex(str[j])) || -1==(c2 = char_to_hex(str[j+1])))
+			return null;
+		if (!bufsize)
+			return null;
+		j += 2
+		slen -= 2
+		buf[i++] = (c1 << 4) | c2
+		bufsize--;
+	}
+
+	return buf
 }
 
 go()
